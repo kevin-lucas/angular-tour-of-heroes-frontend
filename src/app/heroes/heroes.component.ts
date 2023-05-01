2cd082e044e26f43e2dfad8a8810fb221dfdbffb
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2023-05-01 14:15:41 -0300

    detalhe do hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine',
-  };
-
   heroes = HEROES;
   selectedHero?: Hero;
 
